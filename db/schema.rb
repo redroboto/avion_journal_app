faabcdbbfd889ff81d5913856ca8107d698223ac
redroboto <vince.pleyto@gmail.com>
2024-06-07 22:38:33 +0800

    update: same as previous commit, added new column to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_115917) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_07_142328) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_115917) do
     t.datetime "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category_name"
   end
 
   create_table "users", force: :cascade do |t|
