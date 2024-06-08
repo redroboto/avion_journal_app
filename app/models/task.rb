0d5de0a277e2a8857e3abc6896bec239cb0c5e39
redroboto <vince.pleyto@gmail.com>
2024-06-08 10:11:33 +0800

    bug: fixed bug where tasks couldn't save because the columns from Task and Category were not yet properly linked

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
-  belongs_to :categories
+  belongs_to :category
+
+  before_validation :find_or_set_category_id
+
+  private
+
+  def find_or_set_category_id
+    category_on_record = Category.find_by(name: self.category_name)
+
+    if category_on_record
+      self.category_id = category_on_record.id
+    else
+      new_category = Category.create(name: self.category_name)
+      self.category_id = new_category.id
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_142328) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_013110) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_142328) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category_name"
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_142328) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "categories"
 end
