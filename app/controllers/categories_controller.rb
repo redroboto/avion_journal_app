d89ab48f512217923c759c21bb4a2abbb2fdafd4
redroboto <vince.pleyto@gmail.com>
2024-06-11 02:17:01 +0800

    refactor: redefine @category to use current_user helper from devise. Use .build method to create the new category to fix bug where the new category cannot be saved without the User

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
   # GET /categories/1 or /categories/1.json
@@ -22,7 +22,7 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.build(category_params)
 
     respond_to do |format|
       if @category.save
@@ -61,7 +61,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = current_user.categories.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,14 @@ class Category < ApplicationRecord
   has_many :tasks, inverse_of: :category, dependent: :destroy
   belongs_to :user
 
-  accepts_nested_attributes_for :tasks
+  # accepts_nested_attributes_for :tasks
+  # accepts_nested_attributes_for :user
+  
+  # def user_attributes=(attributes)
+  #   if attributes[:id].blank?
+  #     self.user_id = current_user.id
+  #   else
+  #     super(attributes)
+  #   end
+  # end
 end
